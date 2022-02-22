60fc77f42bd6fa714075d5e35349863dd49d9ea0
Alex Donev <alexdonev954@gmail.com>
2022-02-22 16:20:53 +0200

    In file script.js were added comments

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-var password = document.getElementById("password");
-var passnumb = document.getElementById("passnumb"); 
-
 function genPassword() {
+    // Define the basic variables 
     var elements = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var len = document.getElementById("input").value.trim();
-    var len = (len == "" || len == NaN) ? 10 : parseInt(document.getElementById('input').value) - 1;
+    var len = document.getElementById("input").value.trim(); // We use ternary operator to define the var len
+    var len = (len === "" || typeof len === 'string') ? 9 : parseInt(document.getElementById('input').value) - 1; 
     var password = "";
+
+    // We use this loop to generate the combination of symbols
  for (var i = 0; i <= len; i++) {
    var randomNumber = Math.floor(Math.random() * elements.length);
-   password += elements.substring(randomNumber, randomNumber +1);
+   password += elements.substring(randomNumber, randomNumber + 1);
   }
    document.getElementById("password").value = password;
  }
 
 function genPasswordNumb() {
+    // Define the basic variables
     var elements = "0123456789";
     var len = document.getElementById("input").value.trim();
-    var len = (len == "" || len == NaN) ? 10 : parseInt(document.getElementById('input').value) - 1;
+    var len = (len === "" || typeof len == 'string') ? 9 : parseInt(document.getElementById('input').value) - 1;
     var passnumb = "";
- 
+    
+    // Analogical like previous function
     for (var i = 0; i <= len; i++) {
       var randomNumber = Math.floor(Math.random() * elements.length);
-      passnumb += elements.substring(randomNumber, randomNumber +1);
+      passnumb += elements.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById("passnumb").value = passnumb;
  }
 
+// Functions which clear the password's box fields 
 function clearPassword() {
   document.getElementById("password").value = "";
 
@@ -36,6 +39,7 @@ function clearPasswordNumb() {
 
 }
 
+// functions copyPassword() and copyPasswordNumb() are used to copy the generated passwords
 function copyPassword() {
    var copyText = document.getElementById("password");
    copyText.select();
